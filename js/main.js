90d78e29279ae1dc43ceb9d2e319f8411ccac4e0
default <ericjosephfrank@gmail.com>
2019-05-27 23:11:17 -0400

    deleted and modified

--- js/main.js
+++ js/main.js
@@ -172,14 +172,7 @@
     delay: 10,
     time: 1000
   });
-
-  $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
-
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
-  });
-
+  
 
 })(jQuery);
 
